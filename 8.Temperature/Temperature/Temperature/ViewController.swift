f79b51b3ea1d3214bae8fde47f8385a6ac050c60
Taro Minowa <higepon@gmail.com>
2014-09-14 21:07:08 -0700

    Initial Commit

---
+++ 8.Temperature/Temperature/Temperature/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Temperature
+//
+//  Created by Taro Minowa on 9/14/14.
+//  Copyright (c) 2014 Higepon Taro Minowa. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
